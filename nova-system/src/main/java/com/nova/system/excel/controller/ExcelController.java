7040f9b0572a248caeff428b9fb5e648a04aaa65
nirui <626340704@qq.com>
2023-01-29 17:41:17 +0800

    基础功能添加

--- nova-system/src/main/java/com/nova/system/excel/controller/ExcelController.java
+++ nova-system/src/main/java/com/nova/system/excel/controller/ExcelController.java
@@ -1,39 +1,35 @@
 package com.nova.system.excel.controller;
 
 
-import com.nova.common.core.domain.AjaxResult;
-import com.nova.common.util.poi.ExcelUtil;
-import com.nova.system.excel.dto.ExcelCountDTO;
-import com.nova.system.excel.service.ExcelService;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @Description: excel功能控制类
  * @Author: nirui
  * @Date: 2020-05-27
  */
-@Controller
-@RequestMapping("/excel")
+@RestController
 public class ExcelController {
 
-    @Resource
-    private ExcelService excelService;
 
-    @PostMapping("/count")
-    @ResponseBody
-    public AjaxResult count(MultipartFile file) throws Exception {
-        ExcelUtil<ExcelCountDTO> util = new ExcelUtil<ExcelCountDTO>(ExcelCountDTO.class);
-        List<ExcelCountDTO> countList = util.importExcel(file.getInputStream());
-        return AjaxResult.success(countList);
+//    @PostMapping("/count")
+//    public AjaxResult count(MultipartFile file) throws Exception {
+//        ExcelUtil<ExcelCountDTO> util = new ExcelUtil<ExcelCountDTO>(ExcelCountDTO.class);
+//        List<ExcelCountDTO> countList = util.importExcel(file.getInputStream());
+//        return AjaxResult.success(countList);
+//    }
+
+    @GetMapping("/order")
+    public String order()
+    {
+        System.out.println("我进来了 order controller 的 index 方法");
+        return "order controller";
     }
 }
